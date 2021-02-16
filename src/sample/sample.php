076b650d49accf597c1541fddf0e354940c0e776
硯川 千優 Suzurikawa <64934951+SuzurikawaChihiro@users.noreply.github.com>
2021-02-17 00:02:23 +0900

    remove: いらないファイル削除

--- src/sample/sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-$img = ImageCreate(100,100);
-$black = ImageColorAllocate($img, 0x00, 0x00, 0x00);
-ImageFilledRectangle($img, 0,0, 100,100, $black);
-
-header('Content-Type: image/png');
-
-ImagePNG($img);
-?>
